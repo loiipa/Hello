f77542b3b9328e732e64a28dcf95d4d0ee5c4452
loiipa <jch0apple@gmail.com>
2020-04-26 22:26:22 +0900

    3rd

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Oh" + " " +"My"	+ " " +"God");
-		System.out.println("NeXT");
+		System.out.println("NeXT NeXT");
 	}
 }
